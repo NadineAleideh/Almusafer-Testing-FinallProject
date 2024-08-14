42434fbb7d487f1834f182cde36076348127c109
Nadine  Aleideh <nadinealeideh75@gmail.com>
2024-08-14 23:52:08 +0300

    'CheckTheDefaultLangugeIsEnglish-Test'

---
+++ src/HomePage/myTestCases.java
@@ -0,0 +1,38 @@
+package HomePage;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class myTestCases {
+
+	
+	WebDriver driver = new ChromeDriver();
+	String AlmusaferWebsiteURL= "https://global.almosafer.com/en";
+	String ExpectedDefaultLanage = "en";
+	
+	@BeforeTest 
+	
+	public void mySetup() {
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get(AlmusaferWebsiteURL);
+		driver.findElement(By.cssSelector(".sc-jTzLTM.hQpNle.cta__button.cta__saudi.btn.btn-primary")).click();
+	}
+	
+		@Test(priority = 1)
+		public void CheckTheDefaultLangugeIsEnglish() {
+			String ActualLanguage = driver.findElement(By.tagName("html")).getAttribute("lang");
+			
+			Assert.assertEquals(ActualLanguage, ExpectedDefaultLanage);
+		}
+		
+
+	
+}
